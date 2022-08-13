ed7456bdaedd80fd21a579e4b0f9e570aba1edb7
kdaisuke853 <daisukekawakami53@icloud.com>
2022-08-13 23:40:07 +0900

    add: props

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -9,7 +9,9 @@ class ComponentTestController extends Controller
     //
 
     public function showComponent1(){
-        return view('tests.component-test1');
+        $message = 'メッセージ1';
+        return view('tests.component-test1',
+        compact('message'));
     }
 
     public function showComponent2(){
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,7 +1,6 @@
 <x-test.app>
     <x-slot name="header1">aaaa</x-slot>
     テスト1
-
-
-    <x-test.card title="タイトル" content="コンテント"></x-test>
+    <x-test.card title="タイトル" content="コンテント" :message="$message"></x-test>
+    <x-test.card title="タイトル2"></x-tests>
 </x-test.app>
