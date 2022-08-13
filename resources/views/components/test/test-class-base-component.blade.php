6952138c92643f074999d4ad8b4405ba93ba2241
kdaisuke853 <daisukekawakami53@icloud.com>
2022-08-14 01:06:24 +0900

    add: class base

---
+++ resources/views/components/test/test-class-base-component.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <!-- 
+  <options=bold>“ The biggest battle is the war against ignorance. ”</>
+  <fg=gray>— Mustafa Kemal Atatürk</>
+ -->
+ クラスベース
+<div>{{ $classBaseMessage }}</div>
+<div>{{  $defaultMessage }}</div>
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,8 @@
 <x-test.app>
+    <x-slot name="header1">aaaa</x-slot>
     テスト2
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更"></x-test-class-base>
+
 </x-test.app>
