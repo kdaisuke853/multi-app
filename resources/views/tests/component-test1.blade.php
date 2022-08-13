0f7d3c505685dc9979b7853cdeb157b103bf959c
kdaisuke853 <daisukekawakami53@icloud.com>
2022-08-14 00:09:33 +0900

    add: attribute merge

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     テスト1
     <x-test.card title="タイトル" content="コンテント" :message="$message"></x-test>
     <x-test.card title="タイトル2"></x-tests>
+    <x-test.card title="CSS変更" class="bg-red-300"></x-test>
 </x-test.app>
